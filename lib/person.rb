2f694463dfb57e11b0432b7bb90fd8e256b0144f
April Rabkin <april.rabkin@gmail.com>
2014-06-11 12:00:21 -0400

    s

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person < ActiveRecord::Base
 
-  def initialize(person_hash)
-    @name = person_hash[:name]
-    @compliment = person_hash[:compliment]
-  end
+  # def initialize(person_hash)
+  #   @name = person_hash[:name]
+  #   @compliment = person_hash[:compliment]
+  # end
 
   def give_compliment
     "#{name} #{compliment}"
